0666f3dd85de75d0ebd4715c62933dfe06233abc
lakshmiprasanna44 <nlakshmiprasanna44@gmail.com>
2020-07-16 10:13:33 +0530

    error log changes

--- DALayer/Common/ErrorLog.cs
+++ DALayer/Common/ErrorLog.cs
@@ -1,4 +1,5 @@
 ﻿using SCMModels.SCMModels;
+using System;
 using System.Data.SqlClient;
 
 namespace DALayer.Common
@@ -8,7 +9,7 @@ namespace DALayer.Common
 		public void ErrorMessage(string controllername, string methodname, string exception)
 		{
 			YSCMEntities DB = new YSCMEntities();
-			string query = "insert into dbo.ApiErrorLog(ControllerName,MethodName,ExceptionMsg)values('"+controllername+"', '"+methodname+"', '"+exception+"')";
+			string query = "insert into dbo.ApiErrorLog(ControllerName,MethodName,ExceptionMsg,OccuredDate)values('" + controllername+"', '"+methodname+"', '"+exception+ "','"+ DateTime.Now + "')";
 			SqlConnection con = new SqlConnection(DB.Database.Connection.ConnectionString);
 			SqlCommand cmd = new SqlCommand(query, con);
 			con.Open();
